8a971bcb1f8592dcdcf3adc4294e68508b0e59d4
Pulpo-julian <julian.schrodinger@gmail.com>
2025-04-28 21:01:21 -0500

    relaciones de fk tabla compuesta

--- src/models/Prestamo.ts
+++ src/models/Prestamo.ts
@@ -5,8 +5,12 @@ class Prestamo extends Model {}
 
 Prestamo.init(
     {
-        cedulaEstudiante: { type: DataTypes.STRING, primaryKey: true, allowNull: false, autoIncrement: false},
-        idLibro: { type: DataTypes.INTEGER, primaryKey: true, allowNull: false, autoIncrement: false },
+        cedulaEstudiante: { type: DataTypes.STRING, primaryKey: true, allowNull: false, autoIncrement: false,
+            references: { model: 'tbl_estudiante', key: 'id' }
+        },
+        idLibro: { type: DataTypes.INTEGER, primaryKey: true, allowNull: false, autoIncrement: false,
+            references: { model: 'tbl_libro', key: 'id' }
+         },
         fechaInicio: { type: DataTypes.DATEONLY, allowNull: false },
         fechaHasta: { type: DataTypes.DATEONLY, allowNull: false },
         estado: { type: DataTypes.ENUM, values: ['activo', 'inactivo'], allowNull: false }
