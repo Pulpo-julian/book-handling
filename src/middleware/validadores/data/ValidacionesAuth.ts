22e1a59ad739c0c4e4ed471cb046f8aec4993bd3
Pulpo-julian <julian.schrodinger@gmail.com>
2025-05-01 18:46:46 -0500

    se inicia proceso para validar autenticaciones auth

---
+++ src/middleware/validadores/data/ValidacionesAuth.ts
@@ -0,0 +1,7 @@
+import { Request, Response, NextFunction } from "express";
+import ValidadorErrores from "../../../utils/ValidadorErrores";
+import { HttpStatusCode, HttpBookMessages } from "../../../enums/HttpStatus"; 
+
+export function ValidacionesAuth (req:Request, res:Response, next:NextFunction) {
+  
+}
